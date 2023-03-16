f79f739c0e8fa1453a0064ca2b2e7e3b7a251c2a
Michael Parchment <Jahansuoz@protonmail.com>
2023-03-16 15:31:54 -0400

    Add comments

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, request, jsonify, send_from_directory
-from flask_cors import CORS
-import sqlite3
-import os
+import sqlite3 # Import SQLite3 to be used as the database.
+import os # Import os library to be able to deploy the frontend.
 
 app = Flask(__name__, static_folder=os.path.abspath('frontend/build'))
-CORS(app)
 
+# Home page of the CURD app -- uses the static frontend generated with npm build.
 @app.route('/')
 def serve():
     return send_from_directory(app.static_folder, 'index.html')
@@ -16,49 +15,78 @@ def serve_static(path):
 
 @app.route('/users', methods=['POST'])
 def create_user():
-    conn = sqlite3.connect('appdb.db')
+    # Create route of app ...
+    # First, connect to database:
+    database = sqlite3.databaseect('appdb.db')
+    # Get JSON data from frontend:
     new_user = request.get_json()
+    # Extract new user name from JSON:
     name = new_user['name']
+    # Extract new user points from JSON:
     points = new_user['points']
-    conn.execute("INSERT INTO users (name, points) VALUES (?, ?)", (name, points))
-    conn.commit()
+    # Run SQLite3 command to insert new entry into database:
+    database.execute("INSERT INTO users (name, points) VALUES (?, ?)", (name, points))
+    # Save all changes to the database with the commit() function called on the database.
+    database.commit()
+    # Close database connection for security:
+    database.close()
+    # Return message communicating successful create:
     return jsonify({'message': 'New user created successfully'})
 
 @app.route('/users', methods=['GET'])
 def get_users():
-    conn = sqlite3.connect('appdb.db')
-    cursor = conn.execute("SELECT * FROM users")
-    users = []
-    for row in cursor:
-        user = {'id': row[0], 'name': row[1], 'points': row[2]}
-        users.append(user)
+    # Read route of the app ...
+    # Connect to the database:
+    database = sqlite3.databaseect('appdb.db')
+    # Run SQLite3 command with execute() to read all users stored in the table,
+    # Then call fetchall() to convert selected users into a list.
+    users = database.execute("SELECT * FROM users").fetchall()
+    # Close database connection for security:
+    database.close()
+    # Return all users currently stored in the database as JSON to the frontend:
     return jsonify(users)
 
 @app.route('/users/<int:user_id>', methods=['GET'])
 def get_user(user_id):
-    conn = sqlite3.connect('appdb.db')
-    cursor = conn.execute("SELECT * FROM users WHERE id=?", (user_id,))
-    row = cursor.fetchone()
-    if row is None:
+    # Read route for a single user ...
+    # Connect to the database:
+    database = sqlite3.databaseect('appdb.db')
+    user = database.execute("SELECT * FROM users WHERE id=?", (user_id,)).fetchone()
+    # Check if user does not exist in the database:
+    if user is None:
+        # Communicate that user has not been found:
         return jsonify({'message': 'User not found'})
-    else:
-        user = {'id': row[0], 'name': row[1], 'points': row[2]}
-        return jsonify(user)
+    # Place user info in JSON format to be returned:
+    user_info = {'id': user[0], 'name': user[1], 'points': user[2]}
+    # Return user with the given ID from the database:
+    return jsonify(user_info)
 
 @app.route('/users/<int:user_id>', methods=['PUT'])
 def update_user(user_id):
-    conn = sqlite3.connect('appdb.db')
+    # Update route for a single user ...
+    database = sqlite3.databaseect('appdb.db')
+    # Get JSON data from the frontend:
     updated_user = request.get_json()
+    # Extract new name of user:
     name = updated_user['name']
+    # Extract new points of user:
     points = updated_user['points']
-    conn.execute("UPDATE users SET name=?, points=? WHERE id=?", (name, points, user_id))
-    conn.commit()
+    # Run SQLite3 command with execute() to update user with the matching ID:
+    database.execute("UPDATE users SET name=?, points=? WHERE id=?", (name, points, user_id))
+    # Save changes in the database with commit():
+    database.commit()
+    # Communicate to the frontend that user has been successfully updated:
     return jsonify({'message': 'User updated successfully'})
 
 @app.route('/users/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
-    conn = sqlite3.connect('appdb.db')
-    cursor = conn.cursor()
-    cursor.execute("DELETE FROM users WHERE id=?", (user_id,))
-    conn.commit()
+    # Delete route for a single user ...
+    database = sqlite3.databaseect('appdb.db')
+    # Create cursor for the database to execute command:
+    database_cmd = database.cursor()
+    # Run SQLite3 command on the database to delete user with matching ID:
+    database_cmd.execute("DELETE FROM users WHERE id=?", (user_id,))
+    # Save changes in the database with commit:
+    database.commit()
+    # Communicate to the frontend that the user has been successfully deleted:
     return jsonify({'message': 'User deleted successfully'})
